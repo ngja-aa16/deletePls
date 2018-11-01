cd9094a4607971e5aa696129b6f59079176628a7
Tarc <Tarc@B88>
2018-11-01 10:44:30 +0800

    Add a println

--- src/agile/Agile.java
+++ src/agile/Agile.java
@@ -4,6 +4,7 @@ public class Agile {
 
     public static void main(String[] args) {
         System.out.println("Hello world123");
+        System.out.println("Hello world134");        
     }
     
 }
